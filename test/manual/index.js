ef53bd633432136c3a5c98180cfe29716b61aa9a
Dong Nguyen <ndaidong@gmail.com>
2016-04-24 22:18:34 +0700

    Up to 0.1.0
    - Add more manual test script

--- test/manual/index.js
+++ test/manual/index.js
@@ -60,6 +60,16 @@ Promise.all([
   console.log('Promise.all: Done');
 });
 
+var p1 = Promise.resolve(3);
+var p2 = 1337;
+var p3 = new Promise((resolve) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([ p1, p2, p3 ]).then((values) => {
+  console.log(values); // [3, 1337, "foo"]
+});
+
 let arr = [];
 Promise.series([
   (next) => {
